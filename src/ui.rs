61e0f6ace37b4060c3a17d244cc4e4467df3587c
Cynthia <64978191+AlsoSylv@users.noreply.github.com>
2023-01-15 15:29:12 -0500

    UI refactor

--- src/ui.rs
+++ src/ui.rs
@@ -17,6 +17,7 @@ pub struct MyEguiApp {
     rx: Receiver<Results>,
 
     name: String,
+    active_player: String,
     role: String,
     roles_map: HashMap<String, Option<i64>>,
     roles_reversed: HashMap<i64, String>,
@@ -56,6 +57,7 @@ impl Default for MyEguiApp {
             tx,
             rx,
             name: Default::default(),
+            active_player: Default::default(),
             role: "None".to_owned(),
             roles_map,
             roles_reversed,
@@ -76,7 +78,7 @@ impl MyEguiApp {
         let role = &self.role;
         if let Some(role) = self.roles_map.get(role as &str) {
             match_summaries(
-                self.name.clone(),
+                self.active_player.clone(),
                 self.tx.clone(),
                 ctx.clone(),
                 *role,
@@ -84,7 +86,7 @@ impl MyEguiApp {
             );
         } else {
             match_summaries(
-                self.name.clone(),
+                self.active_player.clone(),
                 self.tx.clone(),
                 ctx.clone(),
                 None,
@@ -92,7 +94,7 @@ impl MyEguiApp {
             );
         };
         player_ranks(
-            self.name.clone(),
+            self.active_player.clone(),
             self.tx.clone(),
             ctx.clone(),
             self.client.clone(),
@@ -132,17 +134,40 @@ impl MyEguiApp {
 
     fn update_player(&self, ctx: &egui::Context) {
         update_player(
-            self.name.clone(),
+            self.active_player.clone(),
             self.tx.clone(),
             ctx.clone(),
             self.client.clone(),
         );
         self.update_matches(ctx);
     }
-}
 
-impl eframe::App for MyEguiApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+    fn player_search_bar(&mut self, ui: &mut egui::Ui, ctx: &egui::Context) {
+        let search_bar = ui.text_edit_singleline(&mut self.name);
+        search_bar.request_focus();
+        if search_bar.changed() {
+            self.update_player_suggestion(ctx);
+        };
+
+        if let Some(pain) = self.players.clone() {
+            let players = pain.data.player_info_suggestions;
+            for option in players {
+                if ui
+                    .selectable_value(
+                        &mut self.name,
+                        option.summoner_name.clone().to_string(),
+                        option.summoner_name.clone(),
+                    )
+                    .clicked()
+                {
+                    self.active_player = self.name.clone();
+                    self.update_matches(ctx);
+                };
+            }
+        }
+    }
+
+    fn update_data(&mut self, ctx: &egui::Context) {
         if let Ok(receiver) = self.rx.try_recv() {
             match receiver {
                 Results::MatchSum(match_sums) => match match_sums {
@@ -188,75 +213,67 @@ impl eframe::App for MyEguiApp {
                 },
             }
         }
+    }
+}
 
-        egui::TopBottomPanel::top("top").show(ctx, |ui| {
-            ui.label(format!("Summoner: {}", self.name))
-                .on_hover_ui(|ui| {
+impl eframe::App for MyEguiApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        self.update_data(ctx);
+
+        egui::SidePanel::left("side_panel")
+            .resizable(false)
+            .show(ctx, |ui| {
+                ui.vertical(|ui| {
+                    ui.add_space(5.0);
+
+                    ui.horizontal(|ui| {
+                        ui.label("Roles: ");
+                        egui::ComboBox::from_id_source("roles")
+                            .selected_text(self.role.clone())
+                            .show_ui(ui, |ui| {
+                                for option in ROLES {
+                                    ui.selectable_value(&mut self.role, option.to_string(), option);
+                                }
+                            });
+                    });
+                    ui.add_space(5.0);
+
+                    ui.horizontal(|ui| {
+                        ui.label("Players: ");
+                        egui::ComboBox::from_id_source("player_suggestions")
+                            .selected_text(self.name.clone())
+                            .show_ui(ui, |ui| {
+                                self.player_search_bar(ui, ctx);
+                            });
+                    });
+
+                    ui.separator();
+
+                    ui.label(format!("Summoner: {}", self.active_player));
                     if let Some(rank) = &self.rank {
                         if rank.tier.is_empty() {
                             ui.label("Unranked");
+                            ui.label("LP: None");
+                            ui.label(format!("Queue: {}", rank.queue_type));
                         } else {
                             ui.label(format!("Rank: {} {}", rank.tier, rank.rank));
                             ui.label(format!("LP: {}", rank.lp));
                             ui.label(format!("Queue: {}", rank.queue_type));
                         }
-                    } else {
-                        ui.spinner();
-                    }
-                })
-        });
+                    };
+                });
+
+                ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+                    egui::widgets::global_dark_light_mode_switch(ui);
+                });
+            });
 
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.vertical(|ui| {
                 ui.add_space(5.0);
 
                 ui.horizontal(|ui| {
-                    ui.label("Roles:");
-                    egui::ComboBox::from_id_source("roles")
-                        .selected_text(self.role.clone())
-                        .show_ui(ui, |ui| {
-                            for option in ROLES {
-                                ui.selectable_value(&mut self.role, option.to_string(), option);
-                            }
-                        });
-                });
-
-                ui.add_space(5.0);
-
-                ui.horizontal(|ui| {
-                    ui.label("Player Suggestions:");
-                    egui::ComboBox::from_id_source("players")
-                        .selected_text(self.name.clone())
-                        .show_ui(ui, |ui| {
-                            let search_bar = ui.text_edit_singleline(&mut self.name);
-                            search_bar.request_focus();
-                            if search_bar.changed() {
-                                self.update_player_suggestion(ctx);
-                            };
-
-                            if let Some(pain) = self.players.clone() {
-                                let players = pain.data.player_info_suggestions;
-                                for option in players {
-                                    if ui
-                                        .selectable_value(
-                                            &mut self.name,
-                                            option.summoner_name.clone().to_string(),
-                                            option.summoner_name.clone(),
-                                        )
-                                        .clicked()
-                                    {
-                                        self.update_matches(ctx);
-                                    };
-                                }
-                            }
-                        });
-                });
-
-                ui.add_space(5.0);
-
-                ui.horizontal(|ui| {
                     if ui.button("Refresh Player").clicked() {
-                        // self.update_player(ctx);
                         self.update_matches(ctx);
                     };
 
@@ -264,7 +281,6 @@ impl eframe::App for MyEguiApp {
 
                     if ui.button("Update Player").clicked() {
                         self.update_player(ctx);
-                        // self.update_matches(ctx);
                     };
 
                     ui.add_space(5.0);
@@ -278,6 +294,8 @@ impl eframe::App for MyEguiApp {
                 });
 
                 ui.add_space(5.0);
+                ui.separator();
+                ui.add_space(5.0);
 
                 egui::ScrollArea::vertical()
                     .max_height(ui.available_height() - 50.0)
@@ -286,21 +304,23 @@ impl eframe::App for MyEguiApp {
                             if summeries.is_empty() {
                                 ui.label("No Data For Summoner");
                             } else {
-                                ui.separator();
-
-                                for summary in summeries {
+                                for (num, summary) in summeries.iter().enumerate() {
                                     self.match_page(summary, ui);
-                                    ui.separator();
+                                    if num != summeries.len() - 1 {
+                                        ui.separator();
+                                    }
                                 }
                             }
                         } else {
-                            ui.spinner();
+                            // ui.spinner();
                         }
                     });
 
-                ui.add_space(5.0);
+                ui.separator();
 
-                ui.hyperlink_to("U.GG", "https://u.gg/");
+                ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+                    egui::widgets::global_dark_light_mode_switch(ui);
+                });
             });
         });
     }
