d0fdc5e5cc30232b2373bcb96812817df56973c1
Cynthia <64978191+AlsoSylv@users.noreply.github.com>
2023-07-31 23:18:27 -0400

    code cleanup

--- src/ui.rs
+++ src/ui.rs
@@ -1,10 +1,7 @@
 use crate::structs::{self, MatchSummary, OverallRanking, RankScore};
-use crate::{
-    match_summaries, player_info, player_ranking, player_ranks, update_player,
-    Errors,
-};
+use crate::{match_summaries, player_info, player_ranking, player_ranks, update_player, Errors};
 use chrono::{DateTime, NaiveDateTime, Utc};
-use eframe::egui::{self, RichText, TextureOptions, Vec2};
+use eframe::egui::{self, Label, TextureOptions, Vec2};
 use eframe::epaint::ColorImage;
 use std::collections::HashMap;
 use std::sync::mpsc::{Receiver, Sender};
@@ -12,7 +9,6 @@ use tokio::runtime::{Builder, Runtime};
 
 pub enum Results {
     MatchSum(Result<structs::PlayerMatchSummeries, Errors>),
-    // PlayerSuggestions(Result<structs::PlayerSuggestions, Errors>),
     PlayerUpdate(Result<structs::UpdatePlayer, Errors>),
     ProfileRanks(Result<structs::PlayerRank, Errors>),
     Ranking(Result<structs::PlayerRanking, Errors>),
@@ -21,13 +17,14 @@ pub enum Results {
 }
 
 /// TODO: Replace the hashmaps with bimaps
+/// TODO: Store player data in a sub struct
 pub struct MyEguiApp {
     tx: Sender<Results>,
     rx: Receiver<Results>,
 
     active_player: String,
-    role: String,
-    roles_map: HashMap<String, Option<i8>>,
+    role: u8,
+    roles_map: HashMap<String, i8>,
     roles_reversed: HashMap<i8, String>,
     summeries: Option<Vec<MatchSummary>>,
     rank: Option<RankScore>,
@@ -53,15 +50,16 @@ impl MyEguiApp {
             .unwrap();
 
         let (tx, rx) = std::sync::mpsc::channel();
+        // Todo: Remove explicit None case?
         let roles_map = HashMap::from([
-            ("Top".to_owned(), Some(4)),
-            ("Jungle".to_owned(), Some(1)),
-            ("Mid".to_owned(), Some(5)),
-            ("ADC".to_owned(), Some(3)),
-            ("Support".to_owned(), Some(2)),
-            ("None".to_owned(), None),
+            ("Top".to_owned(), 4),
+            ("Jungle".to_owned(), 1),
+            ("Mid".to_owned(), 5),
+            ("ADC".to_owned(), 3),
+            ("Support".to_owned(), 2),
         ]);
 
+        // Todo: replace with an array
         let roles_reversed = HashMap::from([
             (4, "Top".to_owned()),
             (1, "Jungle".to_owned()),
@@ -77,7 +75,7 @@ impl MyEguiApp {
             tx,
             rx,
             active_player: Default::default(),
-            role: "None".to_owned(),
+            role: 5,
             roles_map,
             roles_reversed,
             summeries: None,
@@ -98,7 +96,7 @@ impl MyEguiApp {
             self.active_player.clone(),
             self.tx.clone(),
             ctx.clone(),
-            self.roles_map[&self.role],
+            self.roles_map.get(ROLES[self.role as usize]).copied(),
             self.client.clone(),
             self.runtime.handle(),
         );
@@ -129,9 +127,7 @@ impl MyEguiApp {
         egui::CollapsingHeader::new(format!(
             "{} {}",
             summary.champion_id,
-            self.roles_reversed
-                .get(&summary.role)
-                .unwrap_or(&String::from("None"))
+            self.roles_reversed[&summary.role]
         ))
         .id_source(summary.match_id)
         .show(ui, |ui| {
@@ -158,30 +154,21 @@ impl MyEguiApp {
         self.update_matches(ctx);
     }
 
-    fn player_search_bar(&mut self, ui: &mut egui::Ui, ctx: &egui::Context) {
-        let search_bar = ui.text_edit_singleline(&mut self.active_player);
-        search_bar.request_focus();
-
-        if search_bar.clicked() {
-            if !self.active_player.is_empty() {
-                self.update_matches(ctx);
-            } else {
-                self.summeries = None;
-            }
-        }
-    }
-
     fn update_data(&mut self, ctx: &egui::Context) {
+        if !self.active_player.is_empty() {
+            self.refresh_enabled = true;
+            self.update_enabled = true;
+        } else {
+            self.refresh_enabled = false;
+            self.update_enabled = false;
+        };
+
         if let Ok(receiver) = self.rx.try_recv() {
             match receiver {
                 Results::MatchSum(match_sums) => match match_sums {
                     Ok(matches) => {
-                        self.summeries = Some(
-                            matches
-                                .data
-                                .fetch_player_match_summaries
-                                .match_summaries,
-                        )
+                        self.summeries =
+                            Some(matches.data.fetch_player_match_summaries.match_summaries)
                     }
                     Err(err) => {
                         dbg!("{:?}", err);
@@ -217,6 +204,7 @@ impl MyEguiApp {
                         dbg!("{:?}", err);
                     }
                 },
+                // Todo: Display this info
                 Results::PlayerInfo(info) => match info {
                     Ok(_) => {}
                     Err(err) => {
@@ -226,9 +214,12 @@ impl MyEguiApp {
                 Results::PlayerIcon(data) => match data {
                     Ok(icon) => {
                         let mut decoder = png::Decoder::new(&*icon);
+                        let headers = decoder
+                            .read_header_info()
+                            .expect("This is always a PNG, so this shouldn't ever fail");
 
-                        let x = decoder.read_header_info().unwrap().height;
-                        let y = decoder.read_header_info().unwrap().width;
+                        let x = headers.height as usize;
+                        let y = headers.width as usize;
 
                         let mut reader = decoder.read_info().unwrap();
 
@@ -238,10 +229,10 @@ impl MyEguiApp {
 
                         let texture = ctx.load_texture(
                             "icon",
-                            ColorImage::from_rgb([x as usize, y as usize], &buf),
+                            ColorImage::from_rgb([x, y], &buf),
                             TextureOptions::LINEAR,
                         );
-                        let _ = self.player_icon.replace(texture);
+                        let _ = self.player_icon.insert(texture);
                     }
                     Err(err) => {
                         dbg!("{:?}", err);
@@ -256,141 +247,126 @@ impl eframe::App for MyEguiApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         self.update_data(ctx);
 
-        egui::TopBottomPanel::bottom("bottom_panel").show(ctx, |ui| {
-            ui.add_space(5.0);
-            ui.with_layout(egui::Layout::left_to_right(egui::Align::LEFT), |ui| {
-                egui::widgets::global_dark_light_mode_switch(ui);
-
-                ui.add_space(5.0);
-
-                let reset_button = ui.button("Reset GUI");
-
-                if reset_button.clicked() {
-                    self.role = "None".to_owned();
-                    self.summeries = None;
-                    self.ranking = None;
-                    self.rank = None;
-                    self.active_player = Default::default();
-                };
-
-                reset_button.on_hover_ui(|ui| {
-                    ui.label("Reset GUI To Defaults");
-                });
-            });
-        });
-
         egui::SidePanel::left("side_panel")
             .resizable(false)
             .show(ctx, |ui| {
                 ui.vertical(|ui| {
-                    let texture: &eframe::epaint::TextureHandle =
-                        self.player_icon.get_or_insert_with(|| {
-                            let bytes: &[u8] = include_bytes!("../0.png");
-                            let mut png = png::Decoder::new(bytes);
+                    ui.add_space(5.0);
+                    ui.horizontal(|ui| {
+                        ui.label("Player: ");
 
-                            let headers = png.read_header_info().unwrap();
-                            let x = headers.height as usize;
-                            let y = headers.width as usize;
+                        let search_bar = ui.text_edit_singleline(&mut self.active_player);
 
-                            let mut reader = png.read_info().unwrap();
-                            let mut buf = vec![0; reader.output_buffer_size()];
-                            reader.next_frame(&mut buf).unwrap();
+                        if search_bar.clicked() {
+                            if !self.active_player.is_empty() {
+                                self.update_matches(ctx);
+                            } else {
+                                self.summeries = None;
+                            }
+                        }
 
-                            ctx.load_texture(
-                                "none",
-                                ColorImage::from_rgb([x, y], &buf),
-                                TextureOptions::LINEAR,
-                            )
-                        });
-
-                    ui.image(texture, Vec2::new(100.0, 100.0));
+                        if search_bar.changed() {
+                            self.summeries = None;
+                            self.player_icon = None;
+                        }
+                    });
 
                     ui.add_space(5.0);
 
                     ui.horizontal(|ui| {
                         ui.label("Roles: ");
+
                         egui::ComboBox::from_id_source("roles")
-                            .selected_text(&self.role)
+                            .selected_text(ROLES[self.role as usize])
                             .show_ui(ui, |ui| {
-                                for option in ROLES {
-                                    ui.selectable_value(&mut self.role, option.to_string(), option);
+                                for (index, text) in ROLES.iter().enumerate() {
+                                    ui.selectable_value(&mut self.role, index as u8, *text);
                                 }
                             });
                     });
 
                     ui.add_space(5.0);
 
-                    ui.horizontal(|ui| {
-                        ui.label(
-                            RichText::new("Player: ").color(egui::Color32::from_rgb(255, 0, 0)),
-                        );
-                        egui::ComboBox::from_id_source("player_suggestions")
-                            .selected_text(self.active_player.clone())
-                            .show_ui(ui, |ui| {
-                                self.player_search_bar(ui, ctx);
-                            });
-                    });
+                    let button =
+                        ui.add_enabled(self.refresh_enabled, egui::Button::new("Refresh Player"));
+
+                    if button.clicked() {
+                        self.update_matches(ctx);
+                    };
 
                     ui.add_space(5.0);
 
-                    ui.horizontal(|ui| {
-                        let button = ui
-                            .add_enabled(self.refresh_enabled, egui::Button::new("Refresh Player"));
+                    let button =
+                        ui.add_enabled(self.update_enabled, egui::Button::new("Update Player"));
 
-                        if button.clicked() {
-                            self.update_matches(ctx);
-                        };
+                    if button.clicked() {
+                        self.update_player(ctx);
+                    };
 
+                    egui::TopBottomPanel::bottom("bottom_panel").show_inside(ui, |ui| {
                         ui.add_space(5.0);
-
-                        let button =
-                            ui.add_enabled(self.update_enabled, egui::Button::new("Update Player"));
-
-                        if button.clicked() {
-                            self.update_player(ctx);
-                        };
+                        ui.with_layout(egui::Layout::left_to_right(egui::Align::LEFT), |ui| {
+                            egui::widgets::global_dark_light_mode_switch(ui);
+                        });
                     });
-
-                    if !self.active_player.is_empty() {
-                        self.refresh_enabled = true;
-                        self.update_enabled = true;
-                    } else {
-                        self.refresh_enabled = false;
-                        self.update_enabled = false;
-                    };
-
-                    ui.separator();
-
-                    ui.label(format!("Summoner: {}", self.active_player));
-                    if let Some(rank) = &self.rank {
-                        if rank.tier.is_empty() {
-                            ui.label("Unranked");
-                            ui.label("LP: None");
-                            ui.label(format!("Wins: {}", rank.wins));
-                            ui.label(format!("Losses: {}", rank.losses));
-                            ui.label(format!("Queue: {}", rank.queue_type));
-                        } else {
-                            ui.label(format!("Rank: {} {}", rank.tier, rank.rank));
-                            ui.label(format!("LP: {}", rank.lp));
-                            ui.label(format!("Wins: {}", rank.wins));
-                            ui.label(format!("Losses: {}", rank.losses));
-                            ui.label(format!("Queue: {}", rank.queue_type));
-                            if let Some(ranking) = &self.ranking {
-                                ui.label(format!("Ranking: {}", ranking.overall_ranking));
-                                ui.label(format!(
-                                    "Top: {:.1$}%",
-                                    (ranking.overall_ranking * 100) as f64
-                                        / (ranking.total_player_count) as f64,
-                                    1
-                                ));
-                            }
-                        }
-                    };
                 });
             });
 
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.with_layout(egui::Layout::top_down(egui::Align::LEFT), |ui| {
+                ui.horizontal(|ui| {
+                    if self.summeries.is_some() {
+                        if let Some(texture) = &self.player_icon {
+                            ui.image(texture, Vec2::splat(50.0));
+                        } else if !self.active_player.is_empty() {
+                            ui.spinner();
+                        }
+
+                        if let Some(rank) = &self.rank {
+                            let (lol_rank, lp, ranking) = if rank.tier.is_empty() {
+                                (
+                                    Label::new("Unranked"),
+                                    Label::new("LP: None"),
+                                    Label::new("Ranking: None"),
+                                )
+                            } else {
+                                (
+                                    Label::new(format!("Wins: {}", rank.wins)),
+                                    Label::new(format!("Losses: {}", rank.losses)),
+                                    if let Some(ranking) = &self.ranking {
+                                        Label::new(format!(
+                                            "Ranking: {} / {}",
+                                            ranking.overall_ranking, ranking.total_player_count
+                                        ))
+                                    } else {
+                                        Label::new("Ranking: None")
+                                    },
+                                )
+                            };
+
+                            ui.vertical(|ui| {
+                                ui.add(lol_rank);
+                                ui.add(lp);
+                                ui.label(format!("Queue: {}", rank.queue_type));
+                            });
+
+                            ui.separator();
+
+                            ui.vertical(|ui| {
+                                ui.label(format!("Wins: {}", rank.wins));
+                                ui.label(format!("Losses: {}", rank.losses));
+                                ui.add(ranking)
+                            });
+                        };
+                    }
+                });
+
+                ui.add_space(5.0);
+
+                if self.summeries.is_some() {
+                    ui.separator();
+                }
+
                 ui.add_space(5.0);
 
                 egui::ScrollArea::vertical()
@@ -400,12 +376,12 @@ impl eframe::App for MyEguiApp {
                             if summeries.is_empty() {
                                 ui.label("No Data For Summoner");
                             } else {
-                                for (num, summary) in summeries.iter().enumerate() {
+                                summeries.iter().enumerate().for_each(|(index, summary)| {
                                     self.match_page(summary, ui);
-                                    if num != summeries.len() - 1 {
+                                    if index != summeries.len() - 1 {
                                         ui.separator();
                                     }
-                                }
+                                });
                             }
                         } else if self.summeries.is_none() && !self.active_player.is_empty() {
                             ui.spinner();
